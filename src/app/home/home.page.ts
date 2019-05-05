63797fe6eb96c4c56f4742024a33f02158b44b0f
PaoCaro <tfrcampos@gmail.com>
2019-05-05 22:51:29 +0100

    Logout met and fix minor bugs

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { LoadingController, ToastController } from '@ionic/angular';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
+  private loading: any;
 
+  constructor(
+    private loadingCtrl: LoadingController,
+    private authService: AuthService,
+    private toastCtrl: ToastController
+  ) { }
+
+  ngOnInit() { }
+
+  async logout() {
+    await this.presentLoading();
+
+    try {
+      await this.authService.logout();
+    } catch (error) {
+      console.error(error);
+    } finally {
+      this.loading.dismiss();
+    }
+  }
+
+  async presentLoading() {
+    this.loading = await this.loadingCtrl.create({ message: 'Espera um pouco...' });
+    return this.loading.present();
+  }
+
+  async presentToast(message: string) {
+    const toast = await this.toastCtrl.create({ message, duration: 2000 });
+    toast.present();
+  }
 }
